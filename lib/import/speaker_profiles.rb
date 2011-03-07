dbf70a9af35948b3dff8880c06661deaf1addc4e
Ted Han <ted@knowtheory.net>
2011-03-06 19:27:58 -0500

    Oops, forgot to include speaker affiliation.

--- lib/import/speaker_profiles.rb
+++ lib/import/speaker_profiles.rb
@@ -7,11 +7,12 @@ Net::HTTP.start("confoo.ca") do |confoo|
     puts "Fetching profile for #{speaker.name}"
     response = confoo.get(speaker.url)
     speaker_page = Nokogiri::HTML(response.body)
+    speaker.affiliation = speaker_page.css('h1 + h3').text.strip
     container = speaker_page.css('div.speakers')
-    speaker.headshot = "http://confoo.ca" + container.css('img:first-child').first.attributes['src'].text
-    speaker.profile  = container.children[container.children.index(container.css('img:first-child').first) + 1].text
+    speaker.photo_url = "http://confoo.ca" + container.css('img:first-child').first.attributes['src'].text
+    speaker.profile  = container.children[container.children.index(container.css('img:first-child').first) + 1].text.strip
     container.css('div.social-medias ul li.twitter').map do |li|
-      speaker.twitter = li.css("a").attributes["href"].split("/").last
+      speaker.twitter = li.css("a").first.attributes["href"].text.split("/").last
     end
     speaker.save
   end
