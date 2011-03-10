e8fe027f2ced3e1078dadbfc2c78962f49d56ad9
Ted Han <ted@knowtheory.net>
2011-03-10 10:44:04 -0500

    I am awesome

--- confoosed.rb
+++ confoosed.rb
@@ -1,17 +1,16 @@
 require File.join(File.dirname(__FILE__), 'init')
 
 get "/" do
-  "<html><h1>Hello! Are you Confoosed?</h1></html>"
+  "<html><h1>Hello! Are you Confoosed?</h1><p><a href=\"#{url("speakers/#{speaker.id}")}\">Speakers</a></p></html>"
 end
 
 get "/schedule/?" do
-  
 end
 
 get "/sessions/?" do
 end
 
-get "/sessions/:id?" do
+get "/sessions/:id" do
   @session = Confoosed::Session.first(:id => params[:id])
   erb :session, :locals => { :session => @session }
 end
@@ -23,10 +22,4 @@ end
 get "/speakers/:id" do # use %r{/speakers/(?<id>\d+)(.html)?} to match .html as well.
   @speaker = Confoosed::Speaker.first(:id=>params[:id])
   erb :speaker, :locals => { :speaker => @speaker }
-end
-
-not_found do
-end
-
-error do
 end
